82cbf45e74e745bfe463335d6e683016adef3095
lagunatlt <lagunatlt@yandex.ru>
2020-07-03 23:56:42 +0400

    ok

--- js/main.js
+++ js/main.js
@@ -7,3 +7,42 @@ $(document).ready(function ($) {
 		}
 	});
 });
+
+$('.slide-brend__items').slick({
+	infinite: true,
+	slidesToShow: 3,
+	slidesToScroll: 1,
+	arrows: true,
+	dots: true,
+	centerMode: true,
+	variableWidth: true,
+	// responsive: [{
+	// 		breakpoint: 961,
+	// 		settings: {
+	// 			slidesToShow: 1,
+	// 			slidesToScroll: 1,
+	// 		}
+	// 	},
+	// 	{
+	// 		breakpoint: 721,
+	// 		settings: {
+	// 			slidesToShow: 1,
+	// 			slidesToScroll: 1,
+	// 		}
+	// 	},
+	// 	{
+	// 		breakpoint: 641,
+	// 		settings: {
+	// 			slidesToShow: 1,
+	// 			slidesToScroll: 1,
+	// 		}
+	// 	},
+	// 	{
+	// 		breakpoint: 320,
+	// 		settings: {
+	// 			slidesToShow: 1,
+	// 			slidesToScroll: 1,
+	// 		}
+	// 	}
+	// ]
+});
