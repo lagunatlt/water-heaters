77f95ec221bd8c12edce9726788435999bce5491
lagunatlt <lagunatlt@yandex.ru>
2020-07-03 10:55:57 +0400

    ok

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 $(document).ready(function ($) {
 	$(window).scroll(function () {
 		if ($(this).scrollTop() > 20) {
-			$('.nav__header--fixed').addClass('fixed-top');
+			$('.nav__fixed').addClass('fixed-top');
 		} else {
-			$('.nav__header--fixed').removeClass('fixed-top');
+			$('.nav__fixed').removeClass('fixed-top');
 		}
 	});
 });
